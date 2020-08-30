5cf8b2df8d9c1c53c08ea0c8b338db8b05cc0d4c
Anton <anton.matrenin@introduct.tech>
2020-08-30 11:09:21 +0300

    add grid configure widget

--- src/components/BackgroundPickerAndDarkModeToggle.tsx
+++ src/components/BackgroundPickerAndDarkModeToggle.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ActionManager } from "../actions/manager";
 import { AppState } from "../types";
 import { DarkModeToggle } from "./DarkModeToggle";
+import { GridSetting } from "./GridSetting";
 
 export const BackgroundPickerAndDarkModeToggle = ({
   appState,
@@ -19,6 +20,14 @@ export const BackgroundPickerAndDarkModeToggle = ({
         value={appState.appearance}
         onChange={(appearance) => {
           setAppState({ appearance });
+        }}
+      />
+    </div>
+    <div style={{ marginInlineStart: "0.25rem" }}>
+      <GridSetting
+        value={appState.gridSize}
+        onChange={(gridSize) => {
+          setAppState({ gridSize: gridSize === 0 ? null : gridSize });
         }}
       />
     </div>
